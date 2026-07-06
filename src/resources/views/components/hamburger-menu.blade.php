da4621a3e02d92aec7770655262bb90a800e0d7c
shoyama1010 <kochisk8@gmail.com>
2026-07-06 20:36:04 +0900

    タイトル名変更

--- src/resources/views/components/hamburger-menu.blade.php
+++ src/resources/views/components/hamburger-menu.blade.php
@@ -8,5 +8,5 @@
         <!-- ハンバーガーメニューの3本線で、spanタグがライン線 -->
         <!-- クリック時にアニメーションで変形 -->
     </div>
-    <h1 class="header__ttl">Rese</h1>
+    <h1 class="header__ttl">StoreRese</h1>
 </div>
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Rese</title>
+    <title>StoreRese</title>
 
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
